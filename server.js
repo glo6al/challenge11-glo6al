4a7f1a4024783b4ed6ea4bd71ce50c561f51fd73
glo6al <towers.a@gmail.com>
2022-07-12 14:04:53 -0400

    add skeleton to routes directories/files

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3002;
+//define port number of the server
+const PORT = process.env.PORT || 3001;
+
+//import api and html routes
+const apiRoutes = require("./");
+
+//add middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Use apiRoutes
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
