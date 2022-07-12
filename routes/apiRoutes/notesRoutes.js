bcf81881600f61f3eea68b0b650e5ad2e4f251a7
glo6al <towers.a@gmail.com>
2022-07-12 14:50:58 -0400

    add psuedo code to all files

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 
 module.exports = router;
+//add const
+
+//add route to get all notes
+
+//add route to get a single note by id
+
+//add route to post a note
+
+//add route to delete a note (bonus)
+
+//export notes router
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,4 +1,8 @@
 const path = require("path");
 const router = require("express").Router();
 
+//get route path for index.html
+
+//get route path for notes.html
+
 module.exports = router;
