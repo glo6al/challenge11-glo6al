3ac3cd5e3f38c32df73b19015c927c7d7a1612bd
glo6al <towers.a@gmail.com>
2022-07-14 15:34:09 -0400

    reverted back to ../ in htmlRoutes paths

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 //get route path for index.html
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 //get route path for notes.html
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
 module.exports = router;
